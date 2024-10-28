9682f9d2258777c9619becf7cb617f28f6945c36
julianast20 <160276228+julianast20@users.noreply.github.com>
2024-10-28 11:28:41 -0500

    Add files via upload

---
+++ src/bootcamps/entities/bootcamp.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Bootcamp {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    phone: number;
+
+    @Column()
+    address: string;
+
+    @Column()
+    topics: string;
+
+    @Column({name:"average_rating"})
+    averageRating: number;
+
+    @Column()
+    createdAt: Date;
+
+}
